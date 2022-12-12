8649dc556ddebb2352e6927c431f4f2242746a1e
tomasbrezina <tomas.brezinaaa@gmail.com>
2022-12-12 19:37:45 +0100

    -

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import Socials from './Socials'
 
 function Header() {
     return (
-        <header>
+        <header id='home'>
             <div className="container header__container">
                 <h5>Hello I'm</h5>
                 <h1>Tomáš Brežina</h1>
