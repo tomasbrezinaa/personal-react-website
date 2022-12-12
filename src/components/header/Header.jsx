1de44604f8fd01aa7e24493bcce7dad2ec8b14eb
tomasbrezina <tomas.brezinaaa@gmail.com>
2022-12-12 18:29:34 +0100

    finishing header element

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./header.css"
+import CTA from './CTA'
+import photo from "../../assets/me.png"
+import Socials from './Socials'
 
 function Header() {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Tomáš Brežina</h1>
+                <h5 className="text-light">Front-end Developer | Web Developer | FIIT STU Student</h5>
+                <CTA />
+                <Socials />
+
+
+                <div className="photo">
+                    <img src={photo} alt="" />
+                </div>
+
+                <a href="#contact" className='scroll__down'>Scroll Down</a>
+            </div>
+        </header>
     )
 }
 
